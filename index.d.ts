65a083e8ccfa2226bc6e51a66763ae0dc129f7ec
linli <lilin@lantunet.com>
2020-06-02 10:25:14 +0800

    update 去除多余字符，package.json依赖peerDependencies改为dependencies

--- index.d.ts
+++ index.d.ts
@@ -4,7 +4,7 @@ import { {{plugin_name}} } from "./ngx";
  * @name {{plugin_name}}
  * @description This plugin just shows the toast demo.
  * @usage
- * \`\`\`typescript
+ * ```typescript
  * import { {{plugin_name}} } from '{{plugin_id}}';
  * private 变量首字母小写
  * constructor(private {{plugin_name}}: {{plugin_name}}) { }
@@ -13,7 +13,7 @@ import { {{plugin_name}} } from "./ngx";
  * 这里变量首字母小写
  * this.{{plugin_name}}.show();
  *
- * \`\`\`
+ * ```
  */
 export declare class {{plugin_name}}Original extends IonicNativePlugin {
   /**
--- ngx/index.d.ts
+++ ngx/index.d.ts
@@ -3,7 +3,7 @@ import { IonicNativePlugin } from '@ionic-native/core';
  * @name {{plugin_name}}
  * @description This plugin just shows the {{plugin_name}} demo.
  * @usage
- * \`\`\`typescript
+ * ```typescript
  * import { {{plugin_name}} } from '{{plugin_id}}';
  *
  * private 变量首字母小写
@@ -13,7 +13,7 @@ import { IonicNativePlugin } from '@ionic-native/core';
  * 这里变量首字母小写
  * this.{{plugin_name}}.show();
  *
- * \`\`\`
+ * ```
  */
 export declare class {{plugin_name}} extends IonicNativePlugin {
   /**
